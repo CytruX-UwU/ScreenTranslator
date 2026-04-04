402e815733e7befc7c828a666b8595eb97d4ebb9
Redrisu <1324177524@qq.com>
2026-04-04 13:10:38 +0900

    能跑 但是快捷键没有效果

--- screen_translator.py
+++ screen_translator.py
@@ -22,8 +22,8 @@ from PIL import Image, ImageDraw, ImageFont
 # 配置（可改为环境变量或单独配置文件）
 # -----------------------------------------------------------------------------
 
-HOTKEY_FULL = "<ctrl>+<shift>+1>"
-HOTKEY_REGION = "<ctrl>+<shift>+2>"
+HOTKEY_FULL = "<ctrl>+<shift>+<1>"
+HOTKEY_REGION = "<ctrl>+<shift>+<2>"
 OCR_MIN_SCORE = 0.35
 FONT_MAX = 22
 FONT_MIN = 10
